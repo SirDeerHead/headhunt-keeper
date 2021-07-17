4e1e98eef79f68a5159650f4fa395822cfd7ab08
SirDeerHead <pawlikowski.daniel@hotmail.com>
2021-07-17 17:25:35 +0200

    💡 Added comments for FLogin

--- headhunt-keeper/src/login/FLogin.java
+++ headhunt-keeper/src/login/FLogin.java
@@ -45,8 +45,11 @@ public class FLogin extends JFrame {
 	 * Create the frame.
 	 */
 	public FLogin() {
-		setLocationRelativeTo(null);
 		setUndecorated(true);
+		/*
+		 * JFrame with contentPane
+		 */
+		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 400);
 		contentPane = new JPanel();
@@ -55,6 +58,11 @@ public class FLogin extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		
+		
+		/*
+		 * JPanel for JTextField for typing Username
+		 */
 		JPanel PnlUsername = new JPanel();
 		PnlUsername.setBounds(171, 156, 250, 40);
 		contentPane.add(PnlUsername);
@@ -66,6 +74,11 @@ public class FLogin extends JFrame {
 		PnlUsername.add(txtUsername);
 		txtUsername.setColumns(10);
 		
+		
+		
+		/*
+		 * JPanel for JPasswordField for typing Password
+		 */
 		JPanel PnlPassword = new JPanel();
 		PnlPassword.setBounds(171, 204, 250, 40);
 		contentPane.add(PnlPassword);
@@ -76,25 +89,40 @@ public class FLogin extends JFrame {
 		pwdPassword.setBounds(10, 10, 230, 19);
 		PnlPassword.add(pwdPassword);
 		
+		
+		
+		/*
+		 * JPanel that will be a Login button
+		 */
 		JPanel PnlBtnLogin = new JPanel();
 		PnlBtnLogin.setBackground(Color.DARK_GRAY);
 		PnlBtnLogin.setBounds(171, 269, 250, 50);
 		contentPane.add(PnlBtnLogin);
 		PnlBtnLogin.setLayout(null);
 		
+		
+		
+		/*
+		 * JLabel for text LOG IN on the Login Panel Button (PnlBtnLogin)
+		 */
 		JLabel lblLogIn = new JLabel("LOG IN");
 		lblLogIn.setForeground(Color.WHITE);
 		lblLogIn.setFont(new Font("Arial", Font.BOLD, 16));
 		lblLogIn.setBounds(92, 10, 60, 30);
 		PnlBtnLogin.add(lblLogIn);
 		
-		JLabel lblNewLabel = new JLabel("X");
-		lblNewLabel.setBackground(new Color(220, 20, 60));
-		lblNewLabel.setForeground(new Color(220, 20, 60));
-		lblNewLabel.setBorder(new LineBorder(new Color(220, 20, 60), 2, true));
-		lblNewLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(561, 10, 25, 25);
-		contentPane.add(lblNewLabel);
+		
+		
+		/*
+		 * JLabel for X button to close application
+		 */
+		JLabel lblX_close = new JLabel("X");
+		lblX_close.setBackground(new Color(220, 20, 60));
+		lblX_close.setForeground(new Color(220, 20, 60));
+		lblX_close.setBorder(new LineBorder(new Color(220, 20, 60), 2, true));
+		lblX_close.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+		lblX_close.setHorizontalAlignment(SwingConstants.CENTER);
+		lblX_close.setBounds(561, 10, 25, 25);
+		contentPane.add(lblX_close);
 	}
 }
